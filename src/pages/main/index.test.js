57f50ddf091f9df6df9457c2d820433879b67550
Igor Sukhinskii <igor.sukhinskii@gmail.com>
2018-11-04 14:06:17 +0200

    Prettier

--- src/pages/main/index.test.js
+++ src/pages/main/index.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from './index';
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./index";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<Main />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
