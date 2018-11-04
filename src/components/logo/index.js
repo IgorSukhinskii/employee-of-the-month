e8487210c70ae5f8e361ad33710c0bdcaa47b206
Igor Sukhinskii <igor.sukhinskii@gmail.com>
2018-11-04 15:08:48 +0200

    Extract logo into a separate component

---
+++ src/components/logo/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import logo from "./logo.svg";
+import styles from "./styles.module.css";
+
+const Logo = () => <img src={logo} className={styles.logo} alt="logo" />;
+
+export default Logo;
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,23 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import styles from "./styles.module.css";
-import Repositories from "./../../components/repositories";
+import Repositories from "../../components/repositories";
+import Logo from "../../components/logo";
 
 const Main = () => (
   <div className={styles.main}>
     <header className={styles.header}>
-      <img src={logo} className={styles.logo} alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className={styles.link}
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <Logo />
     </header>
     <Repositories />
   </div>
