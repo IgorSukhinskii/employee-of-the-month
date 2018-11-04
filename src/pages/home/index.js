6a1439f6d0d2c2076e88d39be3d7a8b95c221d27
Igor Sukhinskii <igor.sukhinskii@gmail.com>
2018-11-04 19:51:19 +0200

    Main -> Home, material-ui

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import styles from "./styles.module.css";
 import Logo from "../../components/logo";
 
-const Main = () => (
-  <div className={styles.main}>
+const Home = () => (
+  <div className={styles.home}>
     <header className={styles.header}>
       <Logo />
     </header>
   </div>
 );
 
-export default Main;
+export default Home;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Main from "./pages/main";
+import Home from "./pages/home";
 import Organization from "./pages/organization";
 
 const Routes = () => (
   <Router>
     <div>
-      <Route exact path="/" component={Main} />
+      <Route exact path="/" component={Home} />
       <Route path="/orgs/:org" component={Organization} />
     </div>
   </Router>
